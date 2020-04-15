35686d26fb58d087ce0ce690ebc70f9091b2c9dd
Robert Kulp <pedalinon@gmail.com>
2020-04-14 23:17:07 -0700

    working on schema

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,3 +2,11 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 const db = knex(config.development);
 
+module.exports = {
+	find,
+};
+
+function find() {
+	return db('northwind.db3');
+	// console.log({ Yes_find: 'find' })
+}
