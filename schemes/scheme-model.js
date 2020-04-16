96854414fbfc6cb63609700791f4a2fae01a3165
Robert Kulp <pedalinon@gmail.com>
2020-04-16 15:26:50 -0700

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -7,6 +7,9 @@ module.exports = {
 	find,
 	findById,
 	findSteps,
+	add,
+	update,
+	remove,
 };
 
 function find() {
@@ -19,9 +22,26 @@ function findById(id) {
 }
 
 function findSteps(id) {
-	console.log({ FindSteps: 'findSteps' })
 	return db('schemes')
 		.join('steps')
-		.select('schemes.id', 'schemes_name', 'steps.instructions', 'steps.step_number')
-		// .where({  });
+		.select('schemes.id as schemesId', 'schemes.scheme_name', 'steps.step_number', 'steps.step_number', 'steps.instructions' )
+		.where({ schemesId: id });
+}
+
+function add(scheme) {
+	return db('schemes')
+		.insert(scheme)
+}
+
+function update(changes, id) {
+	console.log('update')
+	return db('schemes')
+		.where({ id })
+		.update(changes)
+}
+
+function remove(id) {
+	return db('schemes')
+		.where('id', id)
+		.del();
 }
